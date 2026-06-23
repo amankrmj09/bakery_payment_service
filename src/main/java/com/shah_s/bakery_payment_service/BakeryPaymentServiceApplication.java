67be1d73549c034fe985de4ccea1088ef9f458a5
Aman Kumar Maurya <amankrmj@gmail.com>
2026-06-23 20:42:16 +0530

    Add MethodSecurityConfig import to BakeryPaymentServiceApplication

--- src/main/java/com/shah_s/bakery_payment_service/BakeryPaymentServiceApplication.java
+++ src/main/java/com/shah_s/bakery_payment_service/BakeryPaymentServiceApplication.java
@@ -2,12 +2,15 @@ package com.shah_s.bakery_payment_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import org.devofblue.common.security.MethodSecurityConfig;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@Import(MethodSecurityConfig.class)
 @EnableAsync
 @EnableScheduling
 @EnableDiscoveryClient
@@ -19,3 +22,4 @@ public class BakeryPaymentServiceApplication {
     }
 
 }
+
