8f914bf0b3f000190402833b3692e6b8e1681f92
Aman Kumar Maurya <amankrmj@gmail.com>
2026-06-23 20:59:44 +0530

    remove: removed the test container will update later

--- src/test/java/com/shah_s/bakery_payment_service/BakeryPaymentServiceApplicationTests.java
+++ src/test/java/com/shah_s/bakery_payment_service/BakeryPaymentServiceApplicationTests.java
@@ -2,9 +2,6 @@ package com.shah_s.bakery_payment_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
-
-@Import(TestcontainersConfiguration.class)
 @SpringBootTest
 class BakeryPaymentServiceApplicationTests {
 
