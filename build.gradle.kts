c6a9fac6dfbe255b6a3432c8040f65a8f7d59f7d
Aman Kumar Maurya <amankrmj@gmail.com>
2026-06-23 20:23:45 +0530

    Update: Gradle configuration to use springboot version 3.5.15 and spring cloud version 2025.0.3 and added cloud client dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,34 +1,30 @@
 plugins {
     java
-    id("org.springframework.boot") version "3.5.6"
+    id("org.springframework.boot") version "3.5.15"
     id("io.spring.dependency-management") version "1.1.7"
-    id("org.asciidoctor.jvm.convert") version "3.3.2"
+    // id("org.asciidoctor.jvm.convert") version "4.0.3"
 }
 
-group = "com.shah_s"
-version = "0.0.1-SNAPSHOT"
-description = "bakery_payment_service"
+group = "com.dev_of_blue"
+version = "1.0.0"
+description = "bakery_payment_service for payment processing"
 
 java {
     toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
-    }
-}
-
-configurations {
-    compileOnly {
-        extendsFrom(configurations.annotationProcessor.get())
+        languageVersion = JavaLanguageVersion.of(25)
     }
 }
 
 repositories {
     mavenCentral()
+	mavenLocal()
 }
 
-extra["snippetsDir"] = file("build/generated-snippets")
-extra["springCloudVersion"] = "2025.0.0"
+// extra["snippetsDir"] = file("build/generated-snippets")
+extra["springCloudVersion"] = "2025.0.3"
 
 dependencies {
+	implementation("org.devofblue:common-libs:1.0-SNAPSHOT")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-validation")
@@ -37,18 +33,19 @@ dependencies {
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
     implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
     implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.cloud:spring-cloud-starter-config")
     testImplementation("org.springframework.security:spring-security-test")
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
-    runtimeOnly("io.micrometer:micrometer-registry-prometheus")
+    // runtimeOnly("io.micrometer:micrometer-registry-prometheus")
     runtimeOnly("org.postgresql:postgresql")
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.springframework.boot:spring-boot-testcontainers")
+    // testImplementation("org.springframework.boot:spring-boot-testcontainers")
     testImplementation("io.projectreactor:reactor-test")
-    testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
-    testImplementation("org.testcontainers:junit-jupiter")
-    testImplementation("org.testcontainers:postgresql")
+    // testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
+    // testImplementation("org.testcontainers:junit-jupiter")
+    // testImplementation("org.testcontainers:postgresql")
     runtimeOnly("org.springframework.boot:spring-boot-docker-compose")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     
@@ -68,11 +65,11 @@ tasks.withType<Test> {
     useJUnitPlatform()
 }
 
-tasks.test {
-    outputs.dir(project.extra["snippetsDir"]!!)
-}
-
-tasks.asciidoctor {
-    inputs.dir(project.extra["snippetsDir"]!!)
-    dependsOn(tasks.test)
-}
+// tasks.test {
+//     outputs.dir(project.extra["snippetsDir"]!!)
+// }
+//
+// tasks.asciidoctor {
+//     inputs.dir(project.extra["snippetsDir"]!!)
+//     dependsOn(tasks.test)
+// }
