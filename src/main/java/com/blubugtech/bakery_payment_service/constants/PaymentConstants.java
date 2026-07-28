85c1dc286bfbe097aaff8e2c2eed28ca7d239183
Aman Kumar Maurya <amankrmj@gmail.com>
2026-07-28 20:35:52 +0530

    Bump bakery_common_libs version to 2.5.4

--- src/main/java/com/blubugtech/bakery_payment_service/constants/PaymentConstants.java
+++ src/main/java/com/blubugtech/bakery_payment_service/constants/PaymentConstants.java
@@ -1,5 +1,5 @@
 package com.blubugtech.bakery_payment_service.constants;
 
 public final class PaymentConstants {
-    public static final String CURRENCY_USD = "USD";
+    public static final String CURRENCY_CODE = "INR";
 }
