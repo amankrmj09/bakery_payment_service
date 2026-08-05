dd63e62707e5db1b567172ec20f35930b0b66570
Aman Kumar Maurya <amankrmj@gmail.com>
2026-08-05 14:01:59 +0530

    Refactor: Remove Stripe and PayPal payment gateways

--- src/main/java/com/blubugtech/bakery_payment_service/enums/PaymentGatewayProvider.java
+++ src/main/java/com/blubugtech/bakery_payment_service/enums/PaymentGatewayProvider.java
@@ -1,9 +1,6 @@
 package com.blubugtech.bakery_payment_service.enums;
 
 public enum PaymentGatewayProvider {
-    STRIPE,
-    PAYPAL,
-    SQUARE,
     MANUAL,
     MOCK
 }
--- src/main/java/com/blubugtech/bakery_payment_service/integration/payment/StripeGateway.java
+++
@@ -1,2 +0,0 @@
-package com.blubugtech.bakery_payment_service.integration.payment;
-// This file is deprecated. Use StripeAdapter instead.
